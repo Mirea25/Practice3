7e17883f6e00a82610e456780bb23dd537874d12
Anton <antonshin2003@gmail.com>
2023-03-02 23:08:30 +0300

    fourth task

--- app/src/main/java/com/example/pr2/MainActivity.java
+++ app/src/main/java/com/example/pr2/MainActivity.java
@@ -5,16 +5,24 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "PR2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.relative_layout);
+        Button button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i(TAG, "Button clicked");
+            }
+        });
     }
     protected void onRestart(){
         super.onRestart();
@@ -22,5 +30,8 @@ public class MainActivity extends AppCompatActivity {
         editText.setText(getString(R.string.ZXC3));
         ImageView image = findViewById(R.id.imageView3);
         image.setImageResource(R.drawable.ic_launcher_foreground);
+    }
+    public void LogButton(View view){
+        Log.i(TAG, "Button clicked");
     }
 }
